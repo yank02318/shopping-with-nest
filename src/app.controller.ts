fbe8497cef5956b2810a5da53ca65926e0f5ce19
Yanko Chergov <qnkobg98@gmail.com>
2026-04-29 11:17:03 +0300

    updated initial project stage with an app service and changed dev and build scripts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,12 @@
 import { Controller, Get } from "@nestjs/common";
+import { AppService } from "./app.service.js";
 
 @Controller()
 export class AppController {
+	constructor(private readonly appService: AppService) {}
+
 	@Get()
 	getHello(): { message: string } {
-		return { message: "Hello from custom shopping list" };
+		return this.appService.getHello();
 	}
 }
